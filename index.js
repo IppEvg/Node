4473ac8dc3f86699e0f705f5aa9a80fc7cfd3b15
IppEvg <ippevg@mail.ru>
2023-01-16 10:18:56 +0300

    15.01

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
 import EventEmitter from "events"
+import { getEnvironmentData } from "worker_threads";
 
 
 const timeParams = process.argv.slice(2).map((item) => +item); //  «час-день-месяц-год»
 let [hour, day, month, year] = timeParams;
+let different =  new Date(year, month-1, day, hour)-new Date();
+let days = Math.floor(different / 86400000);
+let hours = Math.floor(different % 86400000 /3600000);
+let min = Math.floor((different% 86400000 %3600000) / 60000);
+let sec = Math.floor((different% 86400000 %3600000 % 60000)/1000);
+let actual= new Date();
+let actualYear = actual.getFullYear();
+let actualMonth =actual.getMonth();
+let actualDate =actual.getDate();
+let actualHour = actual.getHours();
 
-let actualYear = new Date().getFullYear();
-let actualMonth = new Date().getMonth();
-let actualDate = new Date().getDate();
-let actualHour = new Date().getHours();
-// 
-let days = new Date() - new Date(year, month, day, hour) / 86400000;
-let hours = new Date() - new Date(year, month, day, hour) % 86400000 3600000;
-let min = new Date() - new Date(year, month, day, hour) % 60000;
-let sec = new Date() - new Date(year, month, day, hour) % 1000;
-console.log(`остаток: ${days} дней, часов,минут, секунд`)
+
 class MyEmitter extends EventEmitter { };
 const emitterObject = new MyEmitter();
 
-
+// class Timer {
+//     constructor(params){
+//         this.
+//     }
+// }
 
 const generateNewTimer = () => {
-    if (actualYear <= year || actualMonth <= month || actualDate <= day || actualHour <= hour) {
-        return setInterval(() => { console.log(new Date() - new Date(year, month, day, hour)) }, 2000);
+    while (
+        (year !== 0 && month!==0&&  day!==0 && hour!==0)||
+        (actualYear <= year || actualMonth <= month || actualDate <= day || actualHour <= hour)
+        ) {
+        return setInterval(() => { console.log(`остаток: ${days} дней,${hours} часов,${min} минут, ${sec} секунд`);
+        if (sec<= 0){
+            min--;
+            sec=60;
+            if(min<=0){
+                hours--;
+                min = 60;
+                if(hours<=0){
+                    days--;
+                    hours=24;
+                    if(days<=0){
+                        console.log("timer is out")
+                        clearInterval(); 
+                    }
+                }
+        
+            }
+        }
+        sec--;
+        },1000);
+        //     return setInterval(() => { console.log(`остаток: ${days} дней,${hours} часов,${min} минут, ${sec} секунд`);
+        //     sec--;
+        // }, 1000);
     }
+    console.log("timer is out")
+
 }
-emitterObject.on('generateNewTimer', generateNewTimer);
+emitterObject.on("newTimer", generateNewTimer);
+
+const run = async () => {
+    generateNewTimer();
+//    await emitterObject.emit();
+    // emitterObject.emit(customer.type, customer.payload);
+    }
+    run();
+    
 //     const intervalValue = generateIntRange(1, 5) * 1000
 //     const params = requestTypes[generateIntRange(0, requestTypes.length - 1)]
 
 //     return delay(intervalValue).then(() => new Customer(params))
 // }
 
-
+// generateNewTimer().then(
+//     customer => emitterObject.emit(customer.type, customer.payload)
+//     );
+    
 
 
 
